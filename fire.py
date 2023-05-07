3d58a9607581baf91065fe850d6572e6aa68dcc7
shiyoung77 <shiyang.lu@rutgers.edu>
2023-05-07 15:59:43 -0400

    Update fire.py.

--- fire.py
+++ fire.py
@@ -1,64 +1,59 @@
+import argparse
 import subprocess
-import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Queue
 from pathlib import Path
+from itertools import repeat
+from time import perf_counter
 
 
-def detic(video, idx):
-    DATASET = Path("~/dataset/ScanNet/aligned_scans").expanduser()
-    VOCABULARY = "lvis"
-    CONFIDENCE_THRESH = 0.3
+global available_devices
 
-    NUM_GPUS = 8
-    DEVICE = idx % NUM_GPUS
-    print(f"Processing {video = }, {idx = }, {DEVICE = }")
+
+def init_process(_available_devices):
+    global available_devices
+    available_devices = _available_devices
+
+
+def detic(dataset, video, idx, args):
+    device = available_devices.get()
+    print(f"Processing {video = }, {idx = }, {device = }")
 
     command = f"""
-        CUDA_VISIBLE_DEVICES={DEVICE} python demo_custom.py \
+        OMP_NUM_THREADS=6 CUDA_VISIBLE_DEVICES={device} python demo_custom.py \
             --config-file "configs/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml" \
-            --dataset "{DATASET}" \
+            --dataset "{dataset}" \
             --video "{video}" \
-            --output_folder "detic_output" \
-            --vocabulary "{VOCABULARY}" \
-            --confidence-threshold {CONFIDENCE_THRESH} \
+            --output_folder "{args.output_folder}" \
+            --vocabulary "{args.vocabulary}" \
+            --confidence-threshold {args.confidence_thresh} \
             --opts MODEL.WEIGHTS "models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth"
     """
     subprocess.run(command, shell=True)
-
-
-def proposed_fusion(video, idx):
-    DATASET = Path("~/t7/ScanNet").expanduser()
-    IOU_THRESH = 0.3
-    RECALL_THRESH = 0.4
-    DEPTH_THRESH = 0.04
-    FEATURE_SIMILARITY_THRESH = 0.7
-    SIZE_THRESH = 100
-    NUM_GPUS = 8
-    DEVICE = f"cuda:{idx % NUM_GPUS}"
-    print(f"Processing {video = }, {idx = }, {DEVICE = }")
-
-    command = f"""
-        python proposed_fusion.py \
-            --dataset "{DATASET}" \
-            --video "{video}" \
-            --detic "scan_net-0.3" \
-            --iou_thresh "{IOU_THRESH}" \
-            --recall_thresh "{RECALL_THRESH}" \
-            --feature_similarity_thresh "{FEATURE_SIMILARITY_THRESH}" \
-            --depth_thresh "{DEPTH_THRESH}" \
-            --size_thresh "{SIZE_THRESH}" \
-            --device "{DEVICE}" \
-            --vocab_feature_file "src/scannet200.npy" \
-    """
-    subprocess.run(command, shell=True)
+    available_devices.put(device)
 
 
 def main():
-    dataset = Path("~/t7/ScanNet/aligned_scans").expanduser()
-    # videos = [i.name for i in sorted(dataset.iterdir())]
-    videos = [i.name for i in sorted(dataset.iterdir()) if i.name.endswith("00")]
-    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
-        # executor.map(proposed_fusion, videos, range(len(videos)))
-        executor.map(proposed_fusion, videos, range(len(videos)))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, default="~/t7/ScanNet/aligned_scans")
+    parser.add_argument("--vocabulary", default="lvis")
+    parser.add_argument("--confidence_thresh", type=float, default=0.3)
+    parser.add_argument("--output_folder", default="detic_output")
+    parser.add_argument("--num_gpus", type=int, default=8)
+    args = parser.parse_args()
+
+    dataset = Path(args.dataset).expanduser()
+    videos = [i.name for i in sorted(dataset.iterdir())]
+
+    _available_devices = Queue()
+    for i in range(args.num_gpus):
+        _available_devices.put(i)
+    init_args = (_available_devices,)
+
+    tic = perf_counter()
+    with ProcessPoolExecutor(max_workers=args.num_gpus, initializer=init_process, initargs=init_args) as executor:
+        executor.map(detic, repeat(dataset), videos, range(len(videos)), repeat(args))
+    print(f"Process {len(videos)} takes {perf_counter() - tic}s")
 
 
 if __name__ == "__main__":
